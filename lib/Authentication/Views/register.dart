8f90ee8d2df166b16851cf1f215565c71cc93ea6
Henrik Kruger <henrikkrugerjensen@gmail.com>
2022-01-01 17:36:36 +0100

    login screen ui in progress

--- lib/Authentication/Views/register.dart
+++ lib/Authentication/Views/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wishlist/constants.dart';
 
 import '../Controllers/authentication_controller.dart';
 
@@ -12,57 +13,106 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final AuthenticationController authController = AuthenticationController.instance;
+  final AuthenticationController authController =
+      AuthenticationController.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Register"),
-      ),
-      body: Column(
-        children: [
-          TextField(
-            decoration: const InputDecoration(hintText: "Email"),
-            controller: _emailController,
-          ),
-          TextField(
-            decoration: const InputDecoration(hintText: "Password"),
-            controller: _passwordController,
-            obscureText: true,
-          ),
-          const SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+      backgroundColor: Colors.white,
+      body: 
+      /*Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.bottomRight,
+          end: Alignment.topLeft,
+          colors: [
+            Constants.gradientBlue,
+            Constants.gradientPurple,
+            Constants.gradientPink
+          ],
+        )),
+        child: */ 
+        SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                onPressed: () async {
-                  authController.register(_emailController.text.trim(),
-                      _passwordController.text.trim());
-                },
-                child: const Text("Sign Up"),
+              const Center(
+                  child: Text(
+                'Welcome to WishList!',
+              )),
+              TextField(
+                decoration: const InputDecoration(hintText: "Email"),
+                controller: _emailController,
               ),
-              ElevatedButton(
-                onPressed: () async {
-                  authController.login(_emailController.text.trim(),
-                      _passwordController.text.trim());
-                },
-                child: const Text("Login"),
+              TextField(
+                decoration: const InputDecoration(hintText: "Password"),
+                controller: _passwordController,
+                obscureText: true,
               ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      authController.register(_emailController.text.trim(),
+                          _passwordController.text.trim());
+                    },
+                    child: const Text("Sign Up"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      authController.login(_emailController.text.trim(),
+                          _passwordController.text.trim());
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Constants.customGreen,
+                      side: const BorderSide(
+                        width: 5.0,
+                        color: Constants.customGreen,
+                      ),
+                    ),
+                    child: const Text("Login"),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              Wrap(
+                spacing: 18,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      authController.signInWithGoogle();
+                    },
+                    child: Container(
+                      height: 32,
+                      width: 32,
+                      decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(16)),
+                      child: Image.asset('assets/google.png'  ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      //authController.signInWithGoogle();
+                    },
+                    child: Container(
+                      height: 32,
+                      width: 32,
+                      decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(16)),
+                      child: Image.asset('assets/facebook2.png'),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.red,
-            ),
-            onPressed: () {
-              authController.signInWithGoogle();
-            },
-            child: const Text("Sign In with Google"),
-          ),
-        ],
-      ),
+        ),
+     // ),
     );
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:wishlist/Authentication/Controllers/authentication_controller.dart';
 
 class Constants {
   static final Future<FirebaseApp> firebaseInitialization = Firebase.initializeApp();
+  static const Color customGreen = Color.fromRGBO(56, 134, 132, 1);
+
+  static const Color gradientBlue = Color.fromRGBO(82, 91, 236, 1);
+  static const Color gradientPurple = Color.fromRGBO(129, 84, 202, 1);
+  static const Color gradientPink = Color.fromRGBO(196, 76, 151, 1);
+
+
 }
